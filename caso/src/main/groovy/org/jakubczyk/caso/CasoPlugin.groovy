9203a72dcf817785ecd75b31985f58103ad9d439
Przemek Jakubczyk <pjakubczyk@applause.com>
2017-08-04 20:10:00 +0200

    Plugin is a groovy file

--- caso/src/main/groovy/org/jakubczyk/caso/CasoPlugin.groovy
+++ caso/src/main/groovy/org/jakubczyk/caso/CasoPlugin.groovy
@@ -3,10 +3,10 @@ package org.jakubczyk.caso;
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
-public class CasoPlugin implements Plugin<Project> {
+class CasoPlugin implements Plugin<Project> {
 
     @Override
-    public void apply(Project target) {
+    void apply(Project target) {
 
     }
 }
