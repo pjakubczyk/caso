9cc970f89d7f29f914d38e8c98005457c6e493d9
Przemek Jakubczyk <pjakubczyk@applause.com>
2017-08-04 20:16:43 +0200

    Added TODO

--- caso/src/main/groovy/org/jakubczyk/caso/CasoPlugin.groovy
+++ caso/src/main/groovy/org/jakubczyk/caso/CasoPlugin.groovy
@@ -7,6 +7,23 @@ class CasoPlugin implements Plugin<Project> {
 
     @Override
     void apply(Project target) {
-
+        /** TODO:
+         1. setup android-sdk directory
+         a) from ANDROID_HOME
+         b) from local.properties
+         c) auto setup
+         2. Apply licenses
+         a) from directory, default "licenses" in rootProject
+         b) from ENVs
+         3. Download android tools
+         a) check for OS and setup links
+         b) download the zip to temp directory
+         c) uncompress to .android-sdk directory
+         d) setup executable
+         4. Create CasoConifugration
+         a) gather which extra repositories to download
+         b) download repositories
+         c) for no configuration download all repositories
+         */
     }
 }
