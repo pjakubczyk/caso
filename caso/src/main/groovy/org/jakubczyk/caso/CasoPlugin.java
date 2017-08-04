f76405fedd55dd1f6005f97bf34c6fb0b6b3ebe2
Przemek Jakubczyk <pjakubczyk@applause.com>
2017-08-04 20:01:29 +0200

    Adding Stub plugin

---
+++ caso/src/main/groovy/org/jakubczyk/caso/CasoPlugin.java
@@ -0,0 +1,12 @@
+package org.jakubczyk.caso;
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+public class CasoPlugin implements Plugin<Project> {
+
+    @Override
+    public void apply(Project target) {
+
+    }
+}
